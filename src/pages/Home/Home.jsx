2d5866169daa88d79c6769114509547eff328604
Sven Dugojevic <sdugoje@gmail.com>
2025-04-11 12:17:35 -0700

    Feat: mocked database

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,33 @@
+import { useEffect, useState } from "react";
 import MetricCard from "../../components/Metric-card/MetricCard";
 import styles from "./Home.module.css";
 
 export default function Home() {
+  const [metrics, setMetrics] = useState([]);
+
+  useEffect(() => {
+    fetchWithDelay("/data/metrics.json", 1000).then((data) => {
+      setMetrics(data);
+    });
+  }, []);
   return (
     <>
       <h1>Welcome Home</h1>
       <div className={styles.cardGrid}>
-        <MetricCard major="437" minor="Active Users" />
-        <MetricCard major="92%" minor="Server Uptime" />
-        <MetricCard major="$8,321" minor="Monthly Revenue" />
-        <MetricCard major="12" minor="Open Support Tickets" />
-        <MetricCard major="4.8★" minor="Customer Rating" />
-        <MetricCard major="28 min" minor="Avg. Session Length" />
-        <MetricCard major="1,203" minor="Total Orders" />
-        <MetricCard major="16 GB" minor="Used Storage" />
-        <MetricCard major="74%" minor="Goal Completion" />
+        {metrics.map((metric, idx) => (
+          <MetricCard key={idx} major={metric.major} minor={metric.minor} />
+        ))}
       </div>
     </>
   );
+}
+
+function fetchWithDelay(url, delay = 1000) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => res.json())
+        .then(resolve);
+    }, delay);
+  });
 }
